93a1fb142c7ea6b1b6085d656f88af99e4a3d321
wen.ma <wen.ma@qunar.com>
2018-06-30 23:12:56 +0800

    添加GBK编码支持

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,32 @@
 extern crate clap;
 use clap::App;
 
-#[macro_use] 
+#[macro_use]
 extern crate prettytable;
 extern crate number_prefix;
+extern crate encoding;
 
 mod parser;
 use parser::{FileContent, TorrentContent, Value};
 
 
 fn main() {
-   let yaml = load_yaml!("../args.yaml");
-   let matches = App::from_yaml(yaml).get_matches();
+    let yaml = load_yaml!("../args.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
 
-   if matches.is_present("analysis") {
-        let content = parser::get_content(matches.value_of("FILE").unwrap()).unwrap();
-        let first_token: u8 = content[0];
+    let content = parser::get_content(matches.value_of("FILE").unwrap()).unwrap();
+    let first_token: u8 = content[0];
 
-        let mut torrent: TorrentContent = TorrentContent::new(content);
-        let res = torrent.decode_func(first_token)(&mut torrent);
+    let mut torrent: TorrentContent = TorrentContent::new(content);
+    let res = torrent.decode_func(first_token)(&mut torrent);
 
+    if matches.is_present("analysis") {
         let mut files = Vec::<FileContent>::new();
         Value::contents(res, &mut files);
+        FileContent::pprint(&files);
 
-        FileContent::pprint(&files);        
-
-   } else {
+    } else {
         println!("Those features are Not Completed yet !");
-   }
+    }
 
 }
-
-
-
-
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
 
+use std::u8;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
@@ -7,9 +8,11 @@ use std::collections::HashMap;
 use prettytable::Table;
 use prettytable::format;
 use number_prefix::{binary_prefix, Standalone, Prefixed};
+use encoding::{Encoding, DecoderTrap};
+use encoding::all::GBK;
 
 
-pub fn get_content(path: &str) -> ::std::io::Result<Vec<u8>> { 
+pub fn get_content(path: &str) -> ::std::io::Result<Vec<u8>> {
     let filename = absolute_path(path).unwrap();
     let mut file = File::open(&filename)?;
     let mut content: Vec<u8> = Vec::new();
@@ -17,15 +20,13 @@ pub fn get_content(path: &str) -> ::std::io::Result<Vec<u8>> {
     Ok(content)
 }
 
-
 fn absolute_path(path: &str) -> Option<String> {
     let p = Path::new(path);
     match p.canonicalize() {
         Ok(buf) => Some(String::from(buf.to_str().unwrap())),
-        Err(_) => None
+        Err(_) => None,
     }
 }
-
 
 fn to_sha1_hex(slice: &[u8]) -> String {
     let mut ret: String = String::new();
@@ -35,24 +36,41 @@ fn to_sha1_hex(slice: &[u8]) -> String {
     ret
 }
 
+fn to_sha1_binary(hex: String) -> Result<Vec<u8>, String> {
+    let mut ret: Vec<u8> = Vec::new();
+    let mut index = 0;
+    while index < hex.len() {
+        if hex.get(index..index + 2).is_none() {
+            return Err("Not a valid hex!".to_string());
+        }
+        match u8::from_str_radix(hex.get(index..index + 2).unwrap(), 16) {
+            Ok(i) => ret.push(i),
+            Err(_) => return Err("Not a valid hex!".to_string())
+        }
+        index += 2;
+    }
+    Ok(ret)
+}
+
 
 #[derive(Debug)]
 pub struct FileContent {
     path: String,
     length: u64,
-    filehash: String
+    filehash: String,
 }
-
 
 impl FileContent {
     pub fn pprint(files: &Vec<FileContent>) {
         let mut table = Table::new();
 
-        table.add_row(row![bFg -> "文件大小", bFg -> "文件名", bFg -> "hash值"]);
+        table.add_row(
+            row![bFg -> "文件大小", bFg -> "文件名", bFg -> "hash值"],
+        );
         for file in files {
             let length = match binary_prefix(file.length as f32) {
                 Prefixed(prefix, n) => format!("{:.2} {}B", n, prefix),
-                Standalone(bytes) => format!("{} bytes", bytes)
+                Standalone(bytes) => format!("{} Bytes", bytes),
             };
             table.add_row(row![length, file.path, file.filehash]);
         }
@@ -63,15 +81,13 @@ impl FileContent {
     }
 }
 
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Value {
     Int(u64),
     Strings(String),
     List(Vec<Value>),
-    Dict(HashMap<String, Value>)
+    Dict(HashMap<String, Value>),
 }
-
 
 impl Value {
     fn get_string(&self) -> String {
@@ -81,70 +97,156 @@ impl Value {
                 for l in list {
                     target += &match l {
                         Value::Strings(s) => s[..].to_string(),
-                        _ => panic!("Error when parse string!")
+                        _ => panic!("Error when parse string!"),
                     };
                     target += "\n";
                 }
                 target
             }
             Value::Strings(s) => s[..].to_string(),
-            _ => panic!("Error when parse string!")
+            _ => panic!("Error when parse string!"),
         }
     }
 
     fn get_int(&self) -> u64 {
         match self {
             Value::Int(n) => *n,
-            _ => panic!("Error when parse int!")
+            _ => panic!("Error when parse int!"),
+        }
+    }
+
+    fn get_dict(&self) -> HashMap<String, Value> {
+        match self {
+            Value::Dict(d) => d.clone(),
+            _ => panic!("Error when parse dict!"),
+        }
+    }
+
+    fn is_single_inner(&self) -> bool {
+        match self {
+            Value::Dict(d) => {
+                if d.get("info".into())
+                    .unwrap().get_dict().contains_key("files") {
+                        false
+                    } else { true }
+            },
+            _ => panic!("Is not a valid torrent file!")
+        }
+    }
+
+    fn single_contents(res: Value, files: &mut Vec<FileContent>) {
+        match res {
+            Value::Dict(d) => {
+                let info = d.get("info".into())
+                            .unwrap().get_dict();
+
+                if info.contains_key("name") || info.contains_key("name.utf-8") {
+
+                    let mut path = info.get("name".into())
+                                       .unwrap().get_string();
+
+                    if info.contains_key("name.utf-8") {
+                        path = info.get("name.utf-8".into())
+                                   .unwrap().get_string();
+                    }
+
+                    files.push(FileContent {
+                        path: {
+                            match to_sha1_binary(path[..].trim().to_string()) {
+                                Ok(v) => {
+                                    match GBK.decode(&v[..], DecoderTrap::Strict) {
+                                        Ok(s) => s,
+                                        Err(_) => path
+                                    }
+                                },
+                                Err(_) => path
+                            }
+                                
+                        },
+                        length: info.get("length".into())
+                            .unwrap_or(&Value::Int(0))
+                            .get_int(),
+
+                        filehash: info.get("filehash".into())
+                            .unwrap_or(&Value::Strings("".into()))
+                            .get_string(),
+                    });
+                }
+            },
+            _ => panic!("Is not a valid torrent file!")
+        }
+    }
+
+    fn multi_contents(res: Value, files: &mut Vec<FileContent>) {
+        match res {
+            Value::List(list) => {
+                for l in list {
+                    Value::multi_contents(l, files);
+                }
+            }
+            Value::Dict(dict) => {
+                if dict.contains_key("path") || dict.contains_key("path.utf-8") {
+                    let mut path = dict.get("path".into())
+                                       .unwrap().get_string();
+
+                    if dict.contains_key("path.utf-8") {
+                        path = dict.get("path.utf-8".into())
+                                   .unwrap().get_string();
+                    }
+                    
+                    if !path.starts_with("____") {
+                        files.push(FileContent {
+                            path: {
+                                match to_sha1_binary(path[..].trim().to_string()) {
+                                    Ok(v) => {
+                                        match GBK.decode(&v[..], DecoderTrap::Strict) {
+                                            Ok(s) => s,
+                                            Err(_) => path
+                                        }
+                                    },
+                                    Err(_) => path
+                                }
+                                
+                            },
+                            length: dict.get("length".into())
+                                .unwrap_or(&Value::Int(0))
+                                .get_int(),
+
+                            filehash: dict.get("filehash".into())
+                                .unwrap_or(&Value::Strings("".into()))
+                                .get_string(),
+                        });
+                    }
+                }
+
+                for (_, v) in dict {
+                    Value::multi_contents(v, files);
+                }
+            }
+            _ => {}
         }
     }
 
     pub fn contents(res: Value, files: &mut Vec<FileContent>) {
-        match res {
-            Value::List(list) => {
-                for l in list {
-                    Value::contents(l, files);
-                }
-            },
-            Value::Dict(dict) => {
-                if dict.contains_key("path") || dict.contains_key("path.utf-8") {
-                    let path = dict.get("path".into())
-                                   .unwrap().get_string();
-
-                    if !path.starts_with("____") {
-                        files.push(FileContent{
-                            path: path,
-                            length: dict.get("length".into())
-                                        .unwrap_or(&Value::Int(0)).get_int(),
-
-                            filehash: dict.get("filehash".into())
-                                          .unwrap_or(&Value::Strings("".into())).get_string()
-                        });
-                    }   
-                }
-                
-                for (_, v) in dict {
-                    Value::contents(v, files);
-                }
-            },
-            _ => {}
+        if res.is_single_inner() {
+            Value::single_contents(res, files);
+        } else {
+            Value::multi_contents(res, files);
         }
     }
 }
 
-
 #[derive(Debug)]
 pub struct TorrentContent {
     content: Vec<u8>,
-    index: usize
+    index: usize,
 }
-
 
 impl TorrentContent {
     pub fn new(content: Vec<u8>) -> Self {
         TorrentContent {
             content: content,
-            index: 0
+            index: 0,
         }
     }
 
@@ -153,8 +255,8 @@ impl TorrentContent {
             b'l' => TorrentContent::decode_list,
             b'd' => TorrentContent::decode_dict,
             b'i' => TorrentContent::decode_int,
-            b'0' ... b'9' => TorrentContent::decode_string,
-            _ => panic!("Unknown token...")
+            b'0'...b'9' => TorrentContent::decode_string,
+            _ => panic!("Unknown token..."),
         }
     }
 
@@ -166,7 +268,7 @@ impl TorrentContent {
             newf += 1;
         }
 
-        let sn = String::from_utf8(self.content[self.index .. newf].to_vec()).unwrap();
+        let sn = String::from_utf8(self.content[self.index..newf].to_vec()).unwrap();
         let n = sn.parse::<u64>().unwrap();
 
         if self.content[self.index] == b'-' {
@@ -176,20 +278,19 @@ impl TorrentContent {
         } else if self.content[self.index] == b'0' && newf != self.index + 1 {
             panic!("Error when decode int!");
         }
-        
+
         self.index = newf + 1;
         Value::Int(n)
 
     }
 
     fn decode_string(&mut self) -> Value {
-
         let mut colon = self.index;
         while self.content[colon] != b':' {
             colon += 1;
         }
 
-        let sn = String::from_utf8(self.content[self.index .. colon].to_vec()).unwrap();
+        let sn = String::from_utf8(self.content[self.index..colon].to_vec()).unwrap();
         let n = sn.parse::<u64>().unwrap() as usize;
 
         if self.content[self.index] == b'0' && colon != self.index + 1 {
@@ -199,9 +300,9 @@ impl TorrentContent {
         colon += 1;
         self.index = colon + n;
 
-        match String::from_utf8(self.content[colon .. self.index].to_vec()) {
+        match String::from_utf8(self.content[colon..self.index].to_vec()) {
             Ok(s) => Value::Strings(s),
-            Err(_) => Value::Strings(to_sha1_hex(&self.content[colon .. self.index]))
+            Err(_) => Value::Strings(to_sha1_hex(&self.content[colon..self.index]))
         }
     }
 
@@ -226,9 +327,12 @@ impl TorrentContent {
             let v = self.decode_string();
             let f = self.decode_func(self.content[self.index])(self);
             match v {
-                Value::Strings(k) => { data.insert(k, f); },
-                _ => {},
+                Value::Strings(k) => {
+                    data.insert(k, f);
+                }
+                _ => {}
             }
+
         }
 
         self.index += 1;
